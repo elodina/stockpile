88d79fe7d726cafd0b0904060e1f12f86a447bca
Alexey Grachov <grachov.alexey@gmail.com>
2015-09-23 11:41:10 +0300

    Cleanup.

--- executor/config.go
+++ executor/config.go
@@ -2,48 +2,11 @@ package stockpile
 
 import (
 	"fmt"
-	"regexp"
-	"time"
 
 	log "github.com/cihub/seelog"
 )
 
 var Logger log.LoggerInterface
-
-var Config *config = &config{
-	FrameworkName:    "go_kafka_client",
-	FrameworkRole:    "*",
-	FrameworkTimeout: 30 * time.Minute,
-	LogLevel:         "info",
-	Storage:          "file:go_kafka_client_mesos.json",
-}
-
-var executorMask = regexp.MustCompile("executor.*")
-
-type config struct {
-	Api              string
-	Master           string
-	FrameworkName    string
-	FrameworkRole    string
-	FrameworkTimeout time.Duration
-	User             string
-	Executor         string
-	LogLevel         string
-	Storage          string
-}
-
-func (c *config) String() string {
-	return fmt.Sprintf(`api:                 %s
-master:              %s
-framework name:      %s
-framework role:      %s
-framework timeout    %s
-user:                %s
-executor:            %s
-log level:           %s
-storage:             %s
-`, c.Api, c.Master, c.FrameworkName, c.FrameworkRole, c.FrameworkTimeout, c.User, c.Executor, c.LogLevel, c.Storage)
-}
 
 func InitLogging(level string) error {
 	config := fmt.Sprintf(`<seelog minlevel="%s">
