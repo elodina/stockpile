633f0f275f77b7d28faac5211650b7195625f2ba
Alexey Grachov <grachov.alexey@gmail.com>
2016-02-10 16:10:21 +0200

    Remove godeps.

--- Godeps/_workspace/src/github.com/stealthly/go_kafka_client/mesos/prepare.sh
+++
@@ -1,4 +0,0 @@
-export KAFKA_PATH=/Users/alexeygrachov/projects/cogniance/kafka_2.10-0.8.2.1
-
-$KAFKA_PATH/bin/zookeeper-server-start.sh $KAFKA_PATH/config/zookeeper.properties 1>zk.out 2>zk.err &
-$KAFKA_PATH/bin/kafka-server-start.sh $KAFKA_PATH/config/server.properties 1>kafka.out 2>kafka.err &
